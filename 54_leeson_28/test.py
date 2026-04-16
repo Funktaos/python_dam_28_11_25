78e3d4daaef4aaaf23d964c17548ad3acb2bb84b
Aleksey Kuritsyn <46819058+SciWake@users.noreply.github.com>
2026-04-16 10:05:42 +0300

    del file

--- 54_leeson_28/test.py
+++
@@ -1,8 +0,0 @@
-import json
-
-# Преобразование Python-объекта в JSON-строку
-data = {"name": "Alice", "age": set((25, 2, 3)), "is_student": False}
-
-json_string = json.dumps(data)  # Преобразование в JSON-строку
-print(type(json_string))
-print(json_string)
